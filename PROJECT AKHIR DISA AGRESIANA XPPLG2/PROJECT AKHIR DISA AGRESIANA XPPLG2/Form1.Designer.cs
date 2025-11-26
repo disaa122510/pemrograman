fe9e2407d65a7345fb3b1e5527d1922586c024d4
disaa122510 <disa258750@student.smkn1kandeman.sch.id>
2025-11-26 09:27:15 +0700

    Add files via upload

---
+++ PROJECT AKHIR DISA AGRESIANA XPPLG2/PROJECT AKHIR DISA AGRESIANA XPPLG2/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace PROJECT_AKHIR_DISA_AGRESIANA_XPPLG2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.crispy = new System.Windows.Forms.CheckBox();
+            this.dada = new System.Windows.Forms.CheckBox();
+            this.sayap = new System.Windows.Forms.CheckBox();
+            this.pahaatasbawah = new System.Windows.Forms.CheckBox();
+            this.dadanasi = new System.Windows.Forms.CheckBox();
+            this.sayapnasi = new System.Windows.Forms.CheckBox();
+            this.pahanasi = new System.Windows.Forms.CheckBox();
+            this.paket = new System.Windows.Forms.CheckBox();
+            this.teh = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.hitungttl = new System.Windows.Forms.Button();
+            this.ttlharga = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.uwang = new System.Windows.Forms.Label();
+            this.ttlbayar = new System.Windows.Forms.TextBox();
+            this.kembalian = new System.Windows.Forms.Label();
+            this.bayar = new System.Windows.Forms.Button();
+            this.txtkembalian = new System.Windows.Forms.Label();
+            this.txtharga = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // crispy
+            // 
+            this.crispy.AutoSize = true;
+            this.crispy.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.crispy.Location = new System.Drawing.Point(56, 127);
+            this.crispy.Name = "crispy";
+            this.crispy.Size = new System.Drawing.Size(196, 20);
+            this.crispy.TabIndex = 0;
+            this.crispy.Text = "Ayam Crispy...............Rp7000";
+            this.crispy.UseVisualStyleBackColor = false;
+            this.crispy.CheckedChanged += new System.EventHandler(this.crispy_CheckedChanged);
+            // 
+            // dada
+            // 
+            this.dada.AutoSize = true;
+            this.dada.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dada.Location = new System.Drawing.Point(56, 154);
+            this.dada.Name = "dada";
+            this.dada.Size = new System.Drawing.Size(192, 20);
+            this.dada.TabIndex = 1;
+            this.dada.Text = "Dada Ayam...............Rp8000";
+            this.dada.UseVisualStyleBackColor = false;
+            // 
+            // sayap
+            // 
+            this.sayap.AutoSize = true;
+            this.sayap.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.sayap.Location = new System.Drawing.Point(56, 181);
+            this.sayap.Name = "sayap";
+            this.sayap.Size = new System.Drawing.Size(198, 20);
+            this.sayap.TabIndex = 2;
+            this.sayap.Text = "Sayap Ayam...............Rp5000";
+            this.sayap.UseVisualStyleBackColor = false;
+            // 
+            // pahaatasbawah
+            // 
+            this.pahaatasbawah.AutoSize = true;
+            this.pahaatasbawah.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pahaatasbawah.Location = new System.Drawing.Point(56, 208);
+            this.pahaatasbawah.Name = "pahaatasbawah";
+            this.pahaatasbawah.Size = new System.Drawing.Size(265, 20);
+            this.pahaatasbawah.TabIndex = 3;
+            this.pahaatasbawah.Text = "Paha Atas/Bawah Ayam...............Rp8000";
+            this.pahaatasbawah.UseVisualStyleBackColor = false;
+            // 
+            // dadanasi
+            // 
+            this.dadanasi.AutoSize = true;
+            this.dadanasi.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dadanasi.Location = new System.Drawing.Point(56, 235);
+            this.dadanasi.Name = "dadanasi";
+            this.dadanasi.Size = new System.Drawing.Size(240, 20);
+            this.dadanasi.TabIndex = 4;
+            this.dadanasi.Text = "Dada Ayam + Nasi...............Rp10000";
+            this.dadanasi.UseVisualStyleBackColor = false;
+            // 
+            // sayapnasi
+            // 
+            this.sayapnasi.AutoSize = true;
+            this.sayapnasi.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.sayapnasi.Location = new System.Drawing.Point(56, 262);
+            this.sayapnasi.Name = "sayapnasi";
+            this.sayapnasi.Size = new System.Drawing.Size(239, 20);
+            this.sayapnasi.TabIndex = 5;
+            this.sayapnasi.Text = "Sayap Ayam + Nasi...............Rp7000";
+            this.sayapnasi.UseVisualStyleBackColor = false;
+            // 
+            // pahanasi
+            // 
+            this.pahanasi.AutoSize = true;
+            this.pahanasi.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pahanasi.Location = new System.Drawing.Point(56, 289);
+            this.pahanasi.Name = "pahanasi";
+            this.pahanasi.Size = new System.Drawing.Size(275, 20);
+            this.pahanasi.TabIndex = 6;
+            this.pahanasi.Text = "Paha Atas/Bawah + Nasi...............Rp10000";
+            this.pahanasi.UseVisualStyleBackColor = false;
+            // 
+            // paket
+            // 
+            this.paket.AutoSize = true;
+            this.paket.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.paket.Location = new System.Drawing.Point(56, 316);
+            this.paket.Name = "paket";
+            this.paket.Size = new System.Drawing.Size(278, 20);
+            this.paket.TabIndex = 7;
+            this.paket.Text = "Paket Ayam + Nasi + Teh...............Rp12000";
+            this.paket.UseVisualStyleBackColor = false;
+            // 
+            // teh
+            // 
+            this.teh.AutoSize = true;
+            this.teh.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.teh.Location = new System.Drawing.Point(56, 343);
+            this.teh.Name = "teh";
+            this.teh.Size = new System.Drawing.Size(233, 20);
+            this.teh.TabIndex = 8;
+            this.teh.Text = "Teh Dingin/Hangat...............Rp3000";
+            this.teh.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Chocolate;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(541, 46);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "FRIED CHICKEN (BELI WOI!!!)";
+            // 
+            // hitungttl
+            // 
+            this.hitungttl.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.hitungttl.Font = new System.Drawing.Font("Broadway", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hitungttl.Location = new System.Drawing.Point(400, 123);
+            this.hitungttl.Name = "hitungttl";
+            this.hitungttl.Size = new System.Drawing.Size(144, 38);
+            this.hitungttl.TabIndex = 10;
+            this.hitungttl.Text = "HITUNG";
+            this.hitungttl.UseVisualStyleBackColor = false;
+            this.hitungttl.Click += new System.EventHandler(this.hitungttl_Click);
+            // 
+            // ttlharga
+            // 
+            this.ttlharga.AutoSize = true;
+            this.ttlharga.Font = new System.Drawing.Font("Broadway", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ttlharga.Location = new System.Drawing.Point(400, 181);
+            this.ttlharga.Name = "ttlharga";
+            this.ttlharga.Size = new System.Drawing.Size(150, 20);
+            this.ttlharga.TabIndex = 11;
+            this.ttlharga.Text = "Total Harga :";
+            this.ttlharga.Click += new System.EventHandler(this.ttlharga_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkRed;
+            this.button1.Font = new System.Drawing.Font("Broadway", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(599, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 39);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Batal";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // uwang
+            // 
+            this.uwang.AutoSize = true;
+            this.uwang.Font = new System.Drawing.Font("Broadway", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uwang.Location = new System.Drawing.Point(400, 224);
+            this.uwang.Name = "uwang";
+            this.uwang.Size = new System.Drawing.Size(160, 22);
+            this.uwang.TabIndex = 13;
+            this.uwang.Text = "Uang Bayar :";
+            this.uwang.Click += new System.EventHandler(this.uwang_Click);
+            // 
+            // ttlbayar
+            // 
+            this.ttlbayar.Location = new System.Drawing.Point(579, 225);
+            this.ttlbayar.Name = "ttlbayar";
+            this.ttlbayar.Size = new System.Drawing.Size(176, 22);
+            this.ttlbayar.TabIndex = 14;
+            this.ttlbayar.TextChanged += new System.EventHandler(this.bayar_TextChanged);
+            // 
+            // kembalian
+            // 
+            this.kembalian.AutoSize = true;
+            this.kembalian.Font = new System.Drawing.Font("Broadway", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kembalian.Location = new System.Drawing.Point(400, 274);
+            this.kembalian.Name = "kembalian";
+            this.kembalian.Size = new System.Drawing.Size(150, 22);
+            this.kembalian.TabIndex = 15;
+            this.kembalian.Text = "Kembalian : ";
+            this.kembalian.Click += new System.EventHandler(this.kembalian_Click);
+            // 
+            // bayar
+            // 
+            this.bayar.BackColor = System.Drawing.Color.LimeGreen;
+            this.bayar.Font = new System.Drawing.Font("Broadway", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bayar.Location = new System.Drawing.Point(404, 316);
+            this.bayar.Name = "bayar";
+            this.bayar.Size = new System.Drawing.Size(118, 47);
+            this.bayar.TabIndex = 16;
+            this.bayar.Text = "Bayar";
+            this.bayar.UseVisualStyleBackColor = false;
+            this.bayar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtkembalian
+            // 
+            this.txtkembalian.AutoSize = true;
+            this.txtkembalian.Font = new System.Drawing.Font("Broadway", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtkembalian.Location = new System.Drawing.Point(556, 274);
+            this.txtkembalian.Name = "txtkembalian";
+            this.txtkembalian.Size = new System.Drawing.Size(23, 22);
+            this.txtkembalian.TabIndex = 17;
+            this.txtkembalian.Text = "0";
+            this.txtkembalian.Click += new System.EventHandler(this.txtkembalian_Click);
+            // 
+            // txtharga
+            // 
+            this.txtharga.AutoSize = true;
+            this.txtharga.Font = new System.Drawing.Font("Broadway", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtharga.Location = new System.Drawing.Point(624, 181);
+            this.txtharga.Name = "txtharga";
+            this.txtharga.Size = new System.Drawing.Size(24, 22);
+            this.txtharga.TabIndex = 18;
+            this.txtharga.Text = "0";
+            this.txtharga.Click += new System.EventHandler(this.txtharga_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Broadway", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(561, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 22);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Rp.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtharga);
+            this.Controls.Add(this.txtkembalian);
+            this.Controls.Add(this.bayar);
+            this.Controls.Add(this.kembalian);
+            this.Controls.Add(this.ttlbayar);
+            this.Controls.Add(this.uwang);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ttlharga);
+            this.Controls.Add(this.hitungttl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.teh);
+            this.Controls.Add(this.paket);
+            this.Controls.Add(this.pahanasi);
+            this.Controls.Add(this.sayapnasi);
+            this.Controls.Add(this.dadanasi);
+            this.Controls.Add(this.pahaatasbawah);
+            this.Controls.Add(this.sayap);
+            this.Controls.Add(this.dada);
+            this.Controls.Add(this.crispy);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox crispy;
+        private System.Windows.Forms.CheckBox dada;
+        private System.Windows.Forms.CheckBox sayap;
+        private System.Windows.Forms.CheckBox pahaatasbawah;
+        private System.Windows.Forms.CheckBox dadanasi;
+        private System.Windows.Forms.CheckBox sayapnasi;
+        private System.Windows.Forms.CheckBox pahanasi;
+        private System.Windows.Forms.CheckBox paket;
+        private System.Windows.Forms.CheckBox teh;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button hitungttl;
+        private System.Windows.Forms.Label ttlharga;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label uwang;
+        private System.Windows.Forms.TextBox ttlbayar;
+        private System.Windows.Forms.Label kembalian;
+        private System.Windows.Forms.Button bayar;
+        private System.Windows.Forms.Label txtkembalian;
+        private System.Windows.Forms.Label txtharga;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PROJECT AKHIR DISA AGRESIANA XPPLG2/PROJECT AKHIR DISA AGRESIANA XPPLG2/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace PROJECT_AKHIR_DISA_AGRESIANA_XPPLG2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void hitungttl_Click(object sender, EventArgs e)
+        {
+            int Total = 0;
+
+            if (crispy.Checked)  Total += 7000;
+            if (dada.Checked)  Total += 8000;
+            if (sayap.Checked)  Total += 5000;
+            if (pahaatasbawah.Checked) Total += 8000;
+            if (dadanasi.Checked) Total += 10000;
+            if (sayapnasi.Checked) Total += 7000;
+            if (pahanasi.Checked) Total += 10000;
+            if (paket.Checked) Total += 12000;
+            if (teh.Checked) Total += 3000;
+
+            txtharga.Text = $"{Total}";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            crispy.Checked = false;
+            dada.Checked = false;
+            sayap.Checked = false;
+            pahaatasbawah.Checked = false;
+            dadanasi.Checked = false;
+            sayapnasi.Checked = false;
+            pahanasi.Checked = false;
+            paket.Checked = false;
+            teh.Checked = false;
+            txtharga.Text = "0";
+            txtkembalian.Text = "0";
+            ttlbayar.Text = "0";
+            
+
+
+        }
+
+        private void kembalian_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bayar_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtharga.Text) && !string.IsNullOrEmpty(ttlbayar.Text))
+            {
+                int uwang = Convert.ToInt32(ttlbayar.Text);
+                //int Total = Convert.ToInt32(lblTotal.Text);
+                int kembalian = uwang - Convert.ToInt32(txtharga.Text);
+                // MessageBox.Show("Kembalian Anda: Rp." + kembalian.ToString());
+                txtkembalian.Text = $"{kembalian}";
+                // menampilkan kembalian pada txtKembalian
+            }
+        }
+
+        private void txtharga_Click(object sender, EventArgs e)
+        {
+            
+            txtharga.Text = "Total Harga" + txtharga.ToString();
+        }
+
+        private void txtkembalian_Click(object sender, EventArgs e)
+        {
+            txtkembalian.Text = "Kembalian" + txtkembalian.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            int bayar = Convert.ToInt32(txtharga.Text);
+            int Total = Convert.ToInt32(ttlbayar.Text);
+            int kembalian = bayar - Total;
+            txtkembalian.Text = kembalian.ToString();
+
+            if (Total < bayar) ;
+
+            {
+                MessageBox.Show("uang anda kurang");
+            }
+
+            if (Total > bayar);
+
+            {
+                MessageBox.Show("Terimakasih telah berkunjung, Awas kau ga beli lagi nanti!");    
+            }
+
+
+        }
+                
+
+
+        private void ttlharga_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uwang_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void crispy_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+} 
